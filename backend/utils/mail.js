658171f981f7068b736c9a930402cd8081c0d245
Kuberan B <kuberanbaluchami@gmail.com>
2026-02-22 22:07:58 +0530

    forgot passowrd ui and api implementation added

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -9,13 +9,13 @@ const transporter = nodemailer.createTransport({
   secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.APP_PASSWORD,
+    pass: process.env.APP_PASS,
   },
 });
 
-export const sendOtpMail = async (to, otp) => {
+export const sendOtpMail = async ({to, otp}) => {
   const info = await transporter.sendMail({
-    from: `Vingo <${process.env.EMAIL}>`, // sender address
+    from: `${process.env.EMAIL}`, // sender address
     to: to,
     subject: "Otp for password reset", // Subject line
     text: `Your OTP for password reset is: ${otp}. Otp reset in 5 minutes`, // Plain-text version of the message
--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -2,16 +2,69 @@
 import React, { useState } from 'react'
 import { IoArrowBackOutline } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { serverUrl } from '../App';
 
 
 function ForgotPassword() {
 
     const [step, setStep] = useState(1);
     const navigate = useNavigate();
-    const [email, setEmail] = useState();
-    const [otp, setOtp] = useState();
-    const [passowrd, setPassword] = useState();
-    const [confirmPassword, setConfirmPassword] = useState();
+    const [email, setEmail] = useState("");
+    const [otp, setOtp] = useState("");
+    const [passowrd, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+
+    const handleSendOtp = async (email) => {
+
+        try {
+            await axios.post(`${serverUrl}/api/auth/send-otp`, { email }, { withCredentials: true });
+            setStep(2);
+
+        } catch (error) {
+            console.log(`Send Otp Error : ${error.response.data.message}`);
+        }
+
+
+    }
+
+    const handleVerifyOtp = async (email, otp) => {
+
+        try {
+
+            await axios.post(`${serverUrl}/api/auth/verify-otp`, { email, otp }, { withCredentials: true });
+            setStep(3);
+
+        } catch (error) {
+            console.log(`Verify Otp Error : ${error.message}`);
+        }
+
+    }
+
+    const handleResetPassword = async (email, password) => {
+
+        if (password !== confirmPassword) {
+            alert("Password and Confirm Password must be same");
+            return;
+        }
+
+        try {
+
+            await axios.post(`${serverUrl}/api/auth/reset-password`, { email, password }, { withCredentials: true });
+            console
+            navigate("/signin");
+
+        } catch (error) {
+            console.log(`Reset Password Error : ${error.message}`);
+        }
+
+    }
+
+
+
+
+
 
     return (
         <div className='w-full min-h-screen bg-[#fff9f6] p-4  flex items-center justify-center ' >
@@ -34,7 +87,7 @@ function ForgotPassword() {
                         <input id='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter you Email' className=' text-sm rounded-sm border w-full border-[#ddd] px-3 py-1.5   placeholder:text-xs  ' type="text" />
 
                     </div>        <div className="flex items-center justify-center mb-4">
-                        <button onClick={() => setStep(2)} className="w-full py-2 px-4 rounded-md font-semibold text-white cursor-pointer bg-[#ff4d2d] hover:bg-[#e64323] transition-colors">
+                        <button onClick={() => handleSendOtp(email)} className="w-full py-2 px-4 rounded-md font-semibold text-white cursor-pointer bg-[#ff4d2d] hover:bg-[#e64323] transition-colors">
                             Send Otp
                         </button>
                     </div>
@@ -52,12 +105,10 @@ function ForgotPassword() {
                             <input id='otp' value={otp} onChange={(e) => setOtp(e.target.value)} placeholder='Enter you Otp' className=' text-sm rounded-sm border w-full border-[#ddd] px-3 py-1.5   placeholder:text-xs  ' type="text" />
 
                         </div>        <div className="flex items-center justify-center mb-4">
-                            <button className="w-full py-2 px-4 rounded-md font-semibold text-white cursor-pointer bg-[#ff4d2d] hover:bg-[#e64323] transition-colors">
-                                Send Otp
+                            <button onClick={() => handleVerifyOtp(email, otp)} className="w-full py-2 px-4 rounded-md font-semibold text-white cursor-pointer bg-[#ff4d2d] hover:bg-[#e64323] transition-colors">
+                                Verify Otp
                             </button>
                         </div>
-
-
 
 
                     </div>
@@ -73,12 +124,12 @@ function ForgotPassword() {
                             <input id='password' value={passowrd} onChange={(e) => setPassword(e.target.value)} placeholder='Enter you password' className=' text-sm rounded-sm border w-full border-[#ddd] px-3 py-1.5   placeholder:text-xs  ' type="text" />
                         </div>
                         <div className='flex flex-col mb-4  '>
-                            <label htmlFor="confirmPassword" className=' font-medium mb-1'>Password</label>
+                            <label htmlFor="confirmPassword" className=' font-medium mb-1'>Confirm Password</label>
                             <input id='confirmPassword' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder='Confirm Password' className=' text-sm rounded-sm border w-full border-[#ddd] px-3 py-1.5   placeholder:text-xs  ' type="text" />
                         </div>
                         <div className="flex items-center justify-center mb-4">
-                            <button className="w-full py-2 px-4 rounded-md font-semibold text-white cursor-pointer bg-[#ff4d2d] hover:bg-[#e64323] transition-colors">
-                                Verify Password
+                            <button onClick={() => handleResetPassword(email, passowrd)} className="w-full py-2 px-4 rounded-md font-semibold text-white cursor-pointer bg-[#ff4d2d] hover:bg-[#e64323] transition-colors">
+                                Reset Password
                             </button>
                         </div>
 
