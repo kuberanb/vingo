3ebf4b1725fe344feeeb911874fcd4859f66b7b8
Kuberan B <kuberanbaluchami@gmail.com>
2026-03-27 01:12:21 +0530

    order api in progress

---
+++ backend/controllers/order.controllers.js
@@ -0,0 +1,111 @@
+import Order from "../models/order.model";
+import Shop from "../models/shop.model";
+import User from "../models/user.model";
+
+export const placeOrder = async (req, res) => {
+  try {
+    const { cartItems, paymentMethod, deliveryAddress } = req.body;
+
+    if (!cartItems || cartItems.length === 0) {
+      return res.status(400).json({ message: "cart is empty" });
+    }
+
+    if (
+      !deliveryAddress.text ||
+      !deliveryAddress.lattitude ||
+      !deliveryAddress.longitude
+    ) {
+      return res.status(400).json({ message: "send complete address" });
+    }
+
+    if (!paymentMethod) {
+      return res.status(400).json({ message: "payment method is required" });
+    }
+
+    const groupItemsbyShop = {};
+
+    cartItems.forEach((item) => {
+      if (!groupItemsbyShop[item.shop]) {
+        groupItemsbyShop[item.shop] = [];
+      }
+      groupItemsbyShop[item.shop].push(item);
+    });
+
+    const shopOrders = await Promise.all(
+      Object.keys(groupItemsbyShop).map(async (shopId) => {
+        const shop = await Shop.findById(shopId).populate("owner");
+        const items = groupItemsbyShop[shopId];
+
+        const shopOrderItems = items.map((i) => ({
+          item: i.id,
+          price: i.price,
+          quantity: i.quantity,
+        }));
+
+        const subTotal = items.reduce((sum, i) => {
+          return sum + i.price * i.quantity;
+        }, 0);
+
+        return {
+          shop: shopId,
+          owner: shop.owner._id,
+          subTotal,
+          shopOrderItems,
+        };
+      }),
+    );
+
+    const totalAmount = shopOrders.reduce((sum, shop) => {
+      return sum + shop.subTotal;
+    }, 0);
+
+    const order = await Order.create({
+      user: req.userId,
+      paymentMethod: paymentMethod,
+      deliveryAddress: {
+        text: deliveryAddress.text,
+        lattitude: deliveryAddress.lattitude,
+        longitude: deliveryAddress.longitude,
+      },
+      totalAmount: totalAmount,
+      shopOrder: shopOrders,
+    });
+
+    await order.populate([
+      { path: "user" },
+      { path: "shopOrder.shop" },
+      { path: "shopOrder.shopOrderItems.item" },
+    ]);
+
+    return res.status(201).json(order);
+  } catch (error) {
+    return res.status(500).json(`place order error : ${error}`);
+  }
+};
+
+export const getOrders = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (user.role === "user") {
+      const userOrders = await Order.find({ user: req.userId }).populate([
+        { path: "user" },
+        { path: "shopOrder.shop" },
+        { path: "shopOrder.shopOrderItems.item" },
+      ]);
+
+      return res.status(200).json(userOrders);
+    } else if (user.role === "owner") {
+      const ownerOrders = await Order.find({
+        "shopOrder.shop": req.userId,
+      }).populate([
+        { path: "user" },
+        { path: "shopOrder.shop" },
+        { path: "shopOrder.shopOrderItems.item" },
+      ]);
+
+      return res.status(200).json(ownerOrders);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: `getOrders error : ${error}` });
+  }
+};
--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -52,5 +52,5 @@ const orderSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-const Order = mongoose.model("Orrder", orderSchema);
+const Order = mongoose.model("Order", orderSchema);
 export default Order;
